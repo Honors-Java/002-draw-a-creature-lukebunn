176e379ec49fe23ec55271e385f6d282314292ad
lukebunn1 <superpantsftw1@gmail.com>
2020-09-18 14:40:39 +0000

    I added the grey color

--- index.java
+++ index.java
@@ -12,13 +12,26 @@ void draw() {
   //Make a creature!!! 
   //all of your programming should be after this 👍
   
- 
+ // I dont know if im supposed to draw the creature on the png or not
+  //the creatures name is bob
+  //create creatures head
+  ellipse(200 , 200, 100, 100)
+  //create creatures eye
+  ellipse(180, 180, 20, 40)
+ //create creature second eye
+  ellipse(220, 180, 20, 40)
+ // create pupil
+  ellipse(180, 180, 10, 20)
+// create pupil(2)
+  ellipse(220, 180, 10, 20)
+// create mouth 
+	rect(200, 220, 70, 1)
   
-  
- 
-
-  
- 
-	
+  fill(100)
+  //create body and legs
+  rect(80, 220, 10, 50)
+  rect(120, 220, 10, 50)
+  rect(110, 200, 80, 40)
 
 }
+
